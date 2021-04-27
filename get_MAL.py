0320384c1a8dfe2f857bbfd9c7bf46d325426b40
jake <jake@localhost.localdomain>
2021-04-27 13:50:44 -0400

    added function to retieve mal_id from top 500 anime

--- get_MAL.py
+++ get_MAL.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 #main_url = 'https://api.jikan.moe/v3/anime/'
 def create_url(anime_num):
@@ -35,3 +36,28 @@ def get_stats(url):
 
     stats = [data['completed'],data['on_hold'],data['plan_to_watch'],data['dropped']]
     return stats, total
+
+# Retrieves top anime numbers from myanimelist.com according to members
+def get_top_anime():
+    all_mal_IDS = []
+    # full_url = 'https://api.jikan.moe/v3/top/anime/1/tv'
+    # response = requests.get(full_url)
+    # data = response.json()
+    for i in range(10):
+        full_url = f'https://api.jikan.moe/v3/top/anime/{i}/tv'
+        response = requests.get(full_url)
+        data = response.json()
+        time.sleep(5)
+
+        for x in range(50):
+            #print(data['top'][x]['mal_id'])
+            all_mal_IDS.append(data['top'][x]['mal_id'])
+
+        with open('MAL_TOP_LIST.txt', 'w') as filehandle:
+            for listitem in all_mal_IDS:
+                filehandle.write('%s\n' % listitem)
+
+    #return all_mal_IDS
+
+
+
